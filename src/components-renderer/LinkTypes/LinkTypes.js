be099bef0363d04ece2220a00a243233de3a0031
Krasen Dimitrov <krasendimitrov@Krasens-MacBook-Pro.local>
2023-09-22 14:10:04 +0300

    making the sizes of the link component

--- src/components-renderer/LinkTypes/LinkTypes.js
+++ src/components-renderer/LinkTypes/LinkTypes.js
@@ -3,14 +3,18 @@ import { LinkButton } from "../../modules";
 export const LinkTypes = () => {
 
     const outContainerClass = 'm-4 border-2 border-black p-4';
-    const innerContainerClass = 'flex justify-between items-baseline m-4';
+    const innerContainerClass = 'flex justify-between items-baseline m-4 bg-gray-200';
 
     return (
         <div>
             <div className={outContainerClass}>
                 <h1>Default</h1>
                 <div className={innerContainerClass}>
-                  <LinkButton path='/buttons'>link</LinkButton>
+                  <LinkButton size='xs' path='/buttons'>Smart home</LinkButton>
+                  <LinkButton size='sm' path='/buttons'>Smart home</LinkButton>
+                  <LinkButton size='base' path='/buttons'>Smart home</LinkButton>
+                  <LinkButton size='lg' path='/buttons'>Smart home</LinkButton>
+                  <LinkButton size='xl' path='/buttons'>Smart home</LinkButton>
                 </div>
             </div>
         </div>
--- src/modules/LinkButton/LinkButton.js
+++ src/modules/LinkButton/LinkButton.js
@@ -2,10 +2,12 @@ import { NavLink } from 'react-router-dom';
 
 import './LinkButton.styles.css';
 
-export const LinkButton = ({ path, variant, children }) => {
+export const LinkButton = ({ path, variant, size, children }) => {
 
     console.log(variant)
-    const classes = 'link-btn-container ' + LinkButton.variants.variant[variant];
+    const classes = 'link-btn-container'
+        + ' ' + LinkButton.variants.variant[variant]
+        + ' ' + LinkButton.variants.size[size];
 
     return (
         <div className={classes}>
@@ -20,11 +22,19 @@ export const LinkButton = ({ path, variant, children }) => {
 LinkButton.defaultProps = {
     variant: 'primary',
     path: '#',
+    size: 'base',
 }
 
 LinkButton.variants = {
     variant: {
         primary: 'link-primary',
         secondary: 'link-secondary',
+    },
+    size: {
+        xs: 'link-xs',
+        sm: 'link-sm',
+        base: 'link-base',
+        lg: 'link-lg',
+        xl: 'link-xl',
     },
 };
