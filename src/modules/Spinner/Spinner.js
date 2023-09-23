51a9a80c50f509d3e5d732a2ac83596f22e715d8
krasenDimitrov90 <krasendimitrov90@gmail.com>
2023-09-23 08:31:48 +0300

    [fix] button position moved when spinner shows up

--- src/modules/Spinner/Spinner.js
+++ src/modules/Spinner/Spinner.js
@@ -1,7 +1,22 @@
 import './Spinner.css';
 
-export const Spinner = () => {
+export const Spinner = ({ size }) => {
+
+    const classes = 'loader ' + 'spinner-' + size;
+
     return (
-        <span className="loader"></span>
+        <span className={classes}></span>
     );
+};
+
+
+
+Spinner.variants = {
+    size: {
+        xs: 'spinner-xs',
+        sm: 'spinner-sm',
+        base: 'spinner-base',
+        lg: 'spinner-lg',
+        xl: 'spinner-xl',
+    },
 };
