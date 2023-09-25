9114873a3f1f96a579e5be9a0535c6b6cea47b00
krasenDimitrov90 <krasendimitrov90@gmail.com>
2023-09-25 18:24:51 +0300

    [FIX] the sliders going beyond one another

--- src/modules/PriceSlider/PriceSlider.js
+++ src/modules/PriceSlider/PriceSlider.js
@@ -6,10 +6,10 @@ export const PriceSlider = ({ min, max, onChange }) => {
     const [mouseIsDown, setMouseIsDown] = React.useState(false);
     const [sliderBar, setSliderBar] = React.useState(null);
     const containerRef = React.useRef(null);
-    const barContainerRef = React.useRef(null);
+    const sliderRef = React.useRef(null);
     const leftBarRef = React.useRef(null);
     const rightBarRef = React.useRef(null);
-    const barsWidth = 16;
+    const barsWidth = 19;
 
     const mouseMoveHandler = (e) => {
 
@@ -43,35 +43,37 @@ export const PriceSlider = ({ min, max, onChange }) => {
 
         // }
 
-        if (mouseIsDown && sliderBar) {
-            if (sliderBar === 'left') {
+        let leftBarX = leftBarRef.current.getBoundingClientRect().x
+        let rightBarX = rightBarRef.current.getBoundingClientRect().x
+        let containerWidth = containerRef.current.offsetWidth;
+        let containerX = containerRef.current.getBoundingClientRect().x;
+        let mouseX = Math.max(e.clientX, containerX);
 
-                let containerWidth = containerRef.current.offsetWidth;
-                let containerX = containerRef.current.getBoundingClientRect().x;
-                let mouseX = Math.max(e.clientX, containerX);
+        // console.log({leftBarX, rightBarX, containerX, containerWidth})
 
-
-                let width = Math.max(((containerX + containerWidth) - mouseX), 0);
-                let left = Math.max(100 - ((width / containerWidth) * 100), 0);
-                barContainerRef.current.style.width = (Math.max((width / containerWidth) * 100, barsWidth)) + '%';
-                barContainerRef.current.style.left = Math.min(left, 100 - barsWidth) + '%';
-                leftBarRef.current.style.left = Math.min(left, 100 - barsWidth) + '%';
-
-            } else {
-
-                let containerWidth = containerRef.current.offsetWidth;
-                let containerX = containerRef.current.getBoundingClientRect().x;
-                let mouseX = Math.max(e.clientX, containerX);
+        if (!mouseIsDown || !sliderBar) return;
 
 
-                let width = Math.max(((containerX + containerWidth) - mouseX), 0);
-                width = Math.max(100 - ((width / containerWidth) * 100), barsWidth);
-                barContainerRef.current.style.width = width + '%';
-                // barContainerRef.current.style.left = 0 + '%';
-                rightBarRef.current.style.right = 100 - width + '%';
-            }
+        if (sliderBar === 'left') {
+            let rightBarLeftPercent = parseFloat(rightBarRef.current.style.left || 100);
+
+            let width = Math.max(((containerX + containerWidth) - mouseX), 0);
+            let left = Math.max(100 - (((width + barsWidth) / containerWidth) * 100), 0);
+            leftBarRef.current.style.left = Math.min(left, rightBarLeftPercent) + '%';
+
+        } else {
+            let leftBarLeftPercent = parseFloat(leftBarRef.current.style.left || 0);
+
+            let width = Math.max(((containerX + containerWidth) - mouseX), 0);
+            let left = Math.max(100 - (((width + barsWidth) / containerWidth) * 100), 0);
+            rightBarRef.current.style.left = Math.max(left, leftBarLeftPercent) + '%';
 
         }
+
+        sliderRef.current.style.width = 
+            parseFloat(rightBarRef.current.style.left || 100) - parseFloat(leftBarRef.current.style.left || 0) + '%';
+        sliderRef.current.style.left = parseFloat(leftBarRef.current.style.left || 0) + '%';
+
     }
 
     const mouseUpHandler = () => {
@@ -91,7 +93,7 @@ export const PriceSlider = ({ min, max, onChange }) => {
     return (
         <div ref={containerRef} className='range-slider-container'>
             <div
-                ref={barContainerRef}
+                ref={sliderRef}
                 className='slider'
             >
             </div>
