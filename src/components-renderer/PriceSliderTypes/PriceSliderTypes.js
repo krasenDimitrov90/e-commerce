10cd353dcc599a4bf3f82431af2a67eefac0ad56
Krasen Dimitrov <krasendimitrov@Krasens-MacBook-Pro.local>
2023-09-30 15:24:22 +0300

    add handler for the min and max price outside of the component and deleted the second Slider implementation

--- src/components-renderer/PriceSliderTypes/PriceSliderTypes.js
+++ src/components-renderer/PriceSliderTypes/PriceSliderTypes.js
@@ -3,46 +3,16 @@ import { PriceSlider } from "../../modules";
 
 export const PriceSliderTypes = () => {
 
-    const [min, setMin] = React.useState(1);
-    const [max, setMax] = React.useState(300);
-    const [minDiffPercent, setMinDiffPercent] = React.useState(0);
-    const [maxDiffPercent, setMaxDiffPercent] = React.useState(100);
-    const maxPrice = 300;
-
-    const getPercent = React.useCallback((val, maxVal) => {
-        return (val / maxVal) * 100;
-    }, []);
-
-    const handleMinChange = React.useCallback((newValue) => {
-        setMin(newValue);
-        setMinDiffPercent(getPercent(newValue, maxPrice));
-    }, []);
-
-    const handleMaxChange = React.useCallback((newValue) => {
-        setMax(newValue);
-        setMaxDiffPercent(getPercent(newValue, maxPrice));
-    }, []);
-
-    const onLeftKnobMove = React.useCallback((newValue) => {
-        setMin(newValue);
-    }, []);
-
-    const onRightKnobMove = React.useCallback((newValue) => {
-        setMax(newValue);
-    }, []);
+    const handlePriceSet = (min, max) => {
+        console.log({ min, max })
+    };
 
     return (
         <div className="w-[300px] m-auto p-5 bg-gray-200 flex justify-center">
             <PriceSlider
-                onLeftKnobMove={onLeftKnobMove}
-                onRightKnobMove={onRightKnobMove}
-                min={min}
-                max={max}
-                minDiffPercent={minDiffPercent}
-                maxDiffPercent={maxDiffPercent}
-                maxPrice={maxPrice}
-                onMinChange={handleMinChange}
-                onMaxChange={handleMaxChange}
+                minPrice={0}
+                maxPrice={1000}
+                handlePriceSet={handlePriceSet}
             />
         </div>
     );
