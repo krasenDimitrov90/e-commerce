9c1fa4a583df84f59f91698e6783f257faf4412a
krasenDimitrov90 <krasendimitrov90@gmail.com>
2023-10-07 13:28:56 +0300

    moved the logic for the different product types in separate components

---
+++ src/modules/ProductCard/ProductCardTypes/Horizontal/Horizontal.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import ProductPrice from '../../ProductPrice/ProductPrice';
+import ProductRating from '../../ProductRating/ProductRating';
+import ProductDescription from '../../ProductDescription/ProductDescription';
+import ProductTumbnails from '../../ProductTumbnails/ProductTumbnails';
+import ProductLabel from '../../ProductLabel/ProductLabel';
+
+import { Image } from '../../../Image/Image';
+import { Button } from '../../../Button/Button';
+import { Icon } from '../../../Icons/Icons';
+
+import './Horizontal.styles.css';
+
+
+export const Horizontal = React.memo((props) => {
+    const {productPath, image, hasPromo, isNew, oldPrice, modalPath, onLike, title, price, onAddToCart} = props
+    return (
+        <div className='product-container-horizontal'>
+            <div className='flex relative'>
+                <NavLink to={productPath} className='product-image-container'>
+                    <Image src={image} />
+                    <ProductLabel hasPromo={hasPromo} isNew={isNew} price={price} oldPrice={oldPrice} />
+                </NavLink>
+                <ProductTumbnails path={modalPath} onLike={onLike} />
+            </div>
+            <div className='product-description-container'>
+                <ProductDescription title={title} path={productPath} />
+                <ProductPrice price={price} oldPrice={oldPrice} />
+                <ProductRating reviews={10} />
+            </div>
+
+            <div className='product-add-to-cart-btn-container'>
+                <Button
+                    onClick={onAddToCart}
+                    variant='primary'
+                    rounded='not-rounded'
+                    size='lg'
+                    expand={true}
+                    leftIcon={<Icon variant='cart' right={true} />}
+                >
+                    Добави в кошницата
+                </Button>
+            </div>
+        </div>
+    )
+});
---
+++ src/modules/ProductCard/ProductCardTypes/index.js
@@ -0,0 +1,8 @@
+/**
+ * Exports different variants components which are supported
+ * by the top level component.
+ **/
+
+export * from './Default';
+export * from './Horizontal/Horizontal';
+export * from './Vertical/Vertical';
