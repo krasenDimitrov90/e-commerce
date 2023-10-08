fb42f83b72fc7a4d1709f7905903eef9679d4192
krasenDimitrov90 <krasendimitrov90@gmail.com>
2023-10-08 17:22:20 +0300

    making the product modal component

--- src/modules/ProductModal/ProductModalTumbnails/ProductModalTumbnails.js
+++ src/modules/ProductModal/ProductModalTumbnails/ProductModalTumbnails.js
@@ -2,18 +2,26 @@ import React from 'react';
 import './ProductModalTumbnails.styles.css';
 import { Image } from '../../Image/Image';
 
-const ProductModalTumbnails = ({ image, allImages }) => {
+const ProductModalTumbnails = ({ allImages }) => {
 
-    const [index, setIdex] = React.useState(0);
+    const [index, setIndex] = React.useState(0);
+
+    const handleIndex = React.useCallback((newIndex) => {
+        setIndex(newIndex || 0);
+    }, []);
+
     return (
         <>
             <div className='product-modal-main-image'>
-                <Image src={image} />
+                <Image src={allImages[index]} />
             </div>
             <div className='product-modal-tumbnails'>
-                {allImages.map(img => {
+                {allImages.map((img, i) => {
                     return (
-                        <div key={img} className="product-modal-tumbnail">
+                        <div
+                            className={`product-modal-tumbnail ${index === i ? 'selected' : ''}`}
+                            onClick={handleIndex.bind(null, i)} key={img + i}
+                        >
                             <Image src={img} />
                         </div>
                     );
--- src/modules/ProductModal/ProductModalTypes/Primary/Primary.js
+++ src/modules/ProductModal/ProductModalTypes/Primary/Primary.js
@@ -5,12 +5,15 @@ import './Primary.styles.css';
 import { Image } from '../../../Image/Image';
 import { Button } from '../../../Button/Button';
 import { Icon } from '../../../Icons/Icons';
+import { Qauantity } from '../../../Qauantity/Qauantity';
+
 import ProductModalTumbnails from '../../ProductModalTumbnails/ProductModalTumbnails';
+import ProductModalPrice from '../ProductModalPrice/ProductModalPrice';
 
 export const Primary = React.memo(() => {
 
     const { productId } = useParams();
-    const [product, setProduct] = React.useState({});
+    const [product, setProduct] = React.useState(null);
 
     React.useEffect(() => {
         fetch('/products.json')
@@ -22,13 +25,46 @@ export const Primary = React.memo(() => {
     }, []);
 
 
-    return (
+    return product && (
         <div className='product-modal'>
             <div className='product-modal-left'>
-                <ProductModalTumbnails image={product.image} allImages={product.allImages || []}/>
+                <ProductModalTumbnails allImages={[product.image, ...(product?.allImages?.slice(1) || [])]} />
             </div>
             <div className='product-modal-right'>
+                <ProductModalPrice price={product.price} oldPrice={product.oldPrice} />
 
+                <div className='text-green-400 my-2'>
+                    <span className='mr-1'>
+                        <i className="fa-regular fa-circle-check"></i>
+                    </span>
+                    <span>На склад</span>
+                </div>
+
+
+                <div className='product-title-container-modal'>
+                    <p>{product.title}</p>
+                </div>
+
+                <Qauantity />
+
+                <div className='product-modal-buttons'>
+                    <Button
+                        expand={true}
+                        size='SQUARE_LG'
+                        rounded='NOT_ROUNDED'
+                    >
+                        Добави в кошницата
+                    </Button>
+                    <Button
+                        // onClick={onLike}
+                        variant={'INFO'}
+                        rounded='NOT_ROUNDED'
+                        size='SQUARE_LG'
+                        fontSize='LG'
+                    >
+                        {<Icon variant='HEART' />}
+                    </Button>
+                </div>
             </div>
         </div>
     );
