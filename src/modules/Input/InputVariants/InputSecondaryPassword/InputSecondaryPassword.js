3b4f817209bc3df7a9eda3a4da3e896d83ad1e0c
krasenDimitrov90 <krasendimitrov90@gmail.com>
2023-10-15 16:54:20 +0300

    making the input secondary password type component

---
+++ src/modules/Input/InputVariants/InputSecondaryPassword/InputSecondaryPassword.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Icon } from '../../../Icons/Icons';
+import './InputSecondaryPassword.styles.css';
+
+export const InputSecondaryPassword = React.memo(() => {
+
+    const [type, setType] = React.useState('password');
+    const [icontVariant, setIcontVariant] = React.useState('EYE_SLAHED');
+
+    const typeHandler = React.useCallback(() => {
+        const newType = type === 'password' ? 'text' : 'password';
+        const newVariant = icontVariant === 'EYE' ? 'EYE_SLAHED' : 'EYE';
+        setType(newType);
+        setIcontVariant(newVariant)
+    }, [type]);
+
+    return (
+        <div className='input-secondary-password-container'>
+            <div className='input-secondary-password-icon'>
+                <Icon variant='UNLOCK' />
+                <div className='input-secondary-password-arrow'>
+                    <i class="fa-solid fa-chevron-right"></i>
+                </div>
+            </div>
+            <input className='input-secondary-password' type={type || 'text'} />
+            <button className='input-secondary-password-eye'
+                onClick={typeHandler}
+            >
+                <Icon variant={icontVariant} />
+            </button>
+        </div>
+    );
+});
