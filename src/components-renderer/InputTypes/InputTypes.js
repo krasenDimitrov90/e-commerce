bfcf0bd3b0ab15dc4ed827634b484c4ae0ebd604
krasenDimitrov90 <krasendimitrov90@gmail.com>
2023-10-15 16:27:26 +0300

    making secondary input component

--- src/components-renderer/InputTypes/InputTypes.js
+++ src/components-renderer/InputTypes/InputTypes.js
@@ -2,12 +2,15 @@ import { Input } from "../../modules";
 
 export const InputTypes = () => {
 
-    const variant = Input.variants.PRIMARY_PASSWORD;
 
     return (
-        <div className="flex flex-col gap-4 h-[220px] w-[500px] p-[50px] bg-gray-200">
-            <Input variant={variant} />
+        <div className="flex flex-col gap-4  w-[500px] p-[50px] bg-gray-200">
+            <span className="text-xs" >PRIMARY</span>
             <Input variant={Input.variants.PRIMARY} />
+            <span className="text-xs" >PRIMARY_PASSWORD</span>
+            <Input variant={Input.variants.PRIMARY_PASSWORD} />
+            <span className="text-xs" >SECONDARY</span>
+            <Input variant={Input.variants.SECONDARY} icon='MAIL' />
         </div>
     );
 };
